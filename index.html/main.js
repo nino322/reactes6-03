82d5b33a520657b0e43b70d060dea969057af384
nino322 <55630809+nino322@users.noreply.github.com>
2024-03-05 22:02:13 +0400

    ლექცია03 დავალება

---
+++ index.html/main.js
@@ -0,0 +1,37 @@
+//დავალება01
+//მოცემულია მასივი რომელშიც მოთავსებულია ობიექტები, საჭიროადაიწეროს ფუნქცია, რომელიც დააბრუნებს მასივში არსებული მანქანების საშუალო ფასს(`price`) - `averagePrice`,
+const obj ={
+ _prop:'9',
+get prop(){
+    return this._prop;
+ },
+  
+}
+console.log(obj.prop); //
+
+//ამ ფუნქციას  გამოვატანენე  ფასი
+
+//დავალება  02
+//დაიწეროს ფუქნცია, რომელიც დააბრუნებს მასივს რომელშიც მხოლოდ ის მანქანები იქნება, რომელთა ფასიც არის 10000-ზე ნაკლები(`price`) - `lessThen10000`
+myFunction();
+
+function myFunction() {
+  let carName = "Volvo";
+  document.getElementById("demo1").innerHTML = typeof carName + " " + carName;
+}
+
+document.getElementById("demo2").innerHTML = typeof carName;
+//scope  ფუნქციით დავაბრუნებნე სახელები
+
+
+//დავალება 03
+// დაიწეროს ფუნქცია, რომელიც დააბრუნებს მასივს რომელშიც იქნება ყველა მანქანა გამოშვებული 2000-2010 წლების შუალედში შემდეგ(`year`) - `producedBetween20002010`
+const car = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+  document.getElementById("demo").innerHTML = cars;
+  //დავალება04
+  //ვერ გავიგე როგორ  უნდა  დავწერო
+
